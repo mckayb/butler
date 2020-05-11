09d3bd160eacf4c64878a96964fb162a59b409f2
McKay Broderick <mckay.db@gmail.com>
2020-05-10 20:49:20 -0600

    Got something working using the knit models

--- src/Butler/Schema.hs
+++ src/Butler/Schema.hs
@@ -1,26 +1,45 @@
-module Butler.Schema (module Knit) where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
--- import Data.Proxy (Proxy(..))
+module Butler.Schema
+    ( module Knit
+    , Schema(..)
+    , Entity(..)
+    -- , GenSchemas(genModel)
+    ) where
+
+import GHC.Generics (Generic, Rep)
+import Butler.TypeInformation
 import Knit
--- import Data.Text (Text)
--- import GHC.Generics (Generic, Rep)
--- import Butler.TypeInformation
--- import qualified Data.Text as Text
+
+-- testShit :: (Show (model m), KnitTables model) => model m -> IO ()
+-- testShit = print
+
+data Schema a = Schema { schemaName :: String, schemaFields :: [Field] } deriving (Show)
+
+class Entity a where
+    schema :: Schema a
+
+    default schema :: (Generic a, Selectors (Rep a)) => Schema a
+    schema = Schema "foo" fields
+        where fields = selectors @(Rep a)
 
 -- data Schema a = Schema { schemaName :: Text, schemaFields :: [Field] }
 
--- class Entity rs a where
-    -- schema :: Schema a
-
-    -- default schema :: (Generic a, Selectors (Rep a), TypeName (Rep a)) => Schema a
-    -- schema = Schema constr fields
-        -- where
+{-
+class Entity rs a where
+    schema :: Schema a
+    default schema :: (Generic a, Selectors (Rep a), TypeName (Rep a)) => Schema a
+    schema = Schema constr fields []
+        where
             -- TODO: How do I parse the relationships from the type?
             -- rels =  show (Proxy :: Proxy rs)
-            -- fields = selectors @(Rep a)
-            -- constr = Text.toLower $ typename @(Rep a)
-
-{-
+            rels = "foo"
+            fields = selectors @(Rep a)
+            constr = Text.toLower $ typename @(Rep a)
 data Comment = Comment { commentContent :: Text }
     deriving (Eq, Show, Generic, Entity '[BelongsTo Post])
 data Post = Post { postTitle :: Text }
--- src/Butler/TypeInformation.hs
+++ src/Butler/TypeInformation.hs
@@ -6,34 +6,47 @@
 {-# LANGUAGE TypeInType #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Butler.TypeInformation where
 
+import Knit
 import Data.Text (Text)
 import Data.Proxy (Proxy)
-import GHC.Generics (U1, M1, D, C, Selector(selName), Datatype(datatypeName), S, K1, R, (:*:))
+import GHC.Generics (Rep, U1, M1, D, C, Selector(selName), Datatype(datatypeName), S, K1, R, (:*:), (:+:))
 import qualified Data.Text as Text
-
 
 data FieldType = Number Int Int | Varchar Int deriving (Show)
 
 class HasFieldType a where
-    toFieldType :: FieldType
+  toFieldType :: FieldType
 
 instance HasFieldType Text where
-    toFieldType = Varchar 255
-
-instance HasFieldType String where
-    toFieldType = Varchar 255
+  toFieldType = Varchar 255
 
 instance HasFieldType Int where
-    toFieldType = Number 11 0
+  toFieldType = Number 11 0
 
 instance HasFieldType a => HasFieldType (Proxy a) where
-    toFieldType = toFieldType @a
+  toFieldType = toFieldType @a
+
+instance HasFieldType a => HasFieldType (Lazy model a) where
+  toFieldType = toFieldType @a
+
+instance HasFieldType a => HasFieldType [Lazy model a] where
+  toFieldType = toFieldType @a
+
+instance HasFieldType a => HasFieldType (RecordId a) where
+  toFieldType = toFieldType @a
+
+instance HasFieldType a => HasFieldType (ForeignRecordId c b a) where
+  toFieldType = toFieldType @a
+
+instance HasFieldType a => HasFieldType [ForeignRecordId c b a] where
+  toFieldType = toFieldType @a
+
 
 type Field = (Text, FieldType)
-
 
 class Selectors rep where
   selectors :: [Field]
@@ -57,6 +70,48 @@ instance (Selector s, HasFieldType t) => Selectors (M1 S s (K1 R t)) where
 
 instance (Selectors a, Selectors b) => Selectors (a :*: b) where
   selectors = selectors @a ++ selectors @b
+
+instance (Selectors a, Selectors b) => Selectors (a :+: b) where
+  selectors = selectors @a ++ selectors @b
+
+instance Selectors (Rep a) => Selectors [a] where
+  selectors = selectors @(Rep a)
+
+
+type TableSchema = [(Text, [Field])]
+
+class ModelSelectors rep where
+  modelSelectors :: TableSchema
+
+instance ModelSelectors U1 where
+  modelSelectors = []
+
+-- | Meta-information (data types, etc.)
+instance ModelSelectors f => ModelSelectors (M1 D x f) where
+  modelSelectors = modelSelectors @f
+
+-- | Meta-information (constructor names, etc.)
+instance ModelSelectors f => ModelSelectors (M1 C x f) where
+  modelSelectors = modelSelectors @f
+
+-- instance (Selector s, Selectors (Rep t)) => ModelSelectors (M1 S s (K1 R t)) where
+  -- modelSelectors =
+    -- [(Text.pack $ selName (undefined :: M1 S s (K1 R t) ()), selectors @(Rep t))]
+
+instance (Selector s, KnitRecord (tables :: Mode -> *) t, Selectors (Rep (t tables mode))) => ModelSelectors (M1 S s (K1 R [t tables mode])) where
+  modelSelectors =
+    [(Text.pack $ selName (undefined :: M1 S s (K1 R (t tables mode)) ()), selectors @(Rep (t tables mode)))]
+
+instance (Selector s, KnitRecord (tables :: Mode -> *) t, Selectors (Rep (t tables mode))) => ModelSelectors (M1 S s (K1 R (t tables mode))) where
+  modelSelectors =
+    [(Text.pack $ selName (undefined :: M1 S s (K1 R (t tables mode)) ()), selectors @(Rep (t tables mode)))]
+
+instance (ModelSelectors a, ModelSelectors b) => ModelSelectors (a :*: b) where
+  modelSelectors = modelSelectors @a ++ modelSelectors @b
+
+instance ModelSelectors (Rep a) => ModelSelectors a where
+  modelSelectors = modelSelectors @(Rep a)
+
   
 class TypeName rep where
   typename :: Text
