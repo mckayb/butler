5c627f171829bcadd78d1942ec9921b8ede319e3
McKay Broderick <mckay.db@gmail.com>
2020-05-11 21:04:40 -0600

    Almost got it. Just need to generate many to many relationships now

--- src/Butler/TypeInformation.hs
+++ src/Butler/TypeInformation.hs
@@ -7,16 +7,20 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-module Butler.TypeInformation where
+module Butler.TypeInformation (ModelSelectors(schema), TableSchema) where
 
 import Knit
 import Data.Text (Text)
-import Data.Proxy (Proxy)
-import GHC.Generics (Rep, U1, M1, D, C, Selector(selName), Datatype(datatypeName), S, K1, R, (:*:), (:+:))
+import Data.Proxy (Proxy(Proxy))
+import GHC.Generics (Rep, U1, M1, D, C, Selector(selName), S, K1, R, (:*:), (:+:))
+import GHC.TypeLits (KnownSymbol, symbolVal)
 import qualified Data.Text as Text
 
-data FieldType = Number Int Int | Varchar Int deriving (Show)
+data FieldType = Number Int Int | Varchar Int | Foreign String String FieldType deriving (Show)
 
 class HasFieldType a where
   toFieldType :: FieldType
@@ -39,11 +43,11 @@ instance HasFieldType a => HasFieldType [Lazy model a] where
 instance HasFieldType a => HasFieldType (RecordId a) where
   toFieldType = toFieldType @a
 
-instance HasFieldType a => HasFieldType (ForeignRecordId c b a) where
-  toFieldType = toFieldType @a
+instance (KnownSymbol table, KnownSymbol field, HasFieldType a) => HasFieldType (ForeignRecordId table field a) where
+  toFieldType = Foreign (symbolVal (Proxy @table)) (symbolVal (Proxy @field)) (toFieldType @a)
 
-instance HasFieldType a => HasFieldType [ForeignRecordId c b a] where
-  toFieldType = toFieldType @a
+instance (KnownSymbol table, KnownSymbol field, HasFieldType a) => HasFieldType [ForeignRecordId table field a] where
+  toFieldType = Foreign (symbolVal (Proxy @table)) (symbolVal (Proxy @field)) (toFieldType @a)
 
 
 type Field = (Text, FieldType)
@@ -74,16 +78,27 @@ instance (Selectors a, Selectors b) => Selectors (a :*: b) where
 instance (Selectors a, Selectors b) => Selectors (a :+: b) where
   selectors = selectors @a ++ selectors @b
 
+instance Selectors (Rep a) => Selectors a where
+  selectors = selectors @(Rep a)
+
 instance Selectors (Rep a) => Selectors [a] where
   selectors = selectors @(Rep a)
+
+instance (KnitRecord (tables :: Mode -> *) t, Selectors (Rep (t tables 'Unresolved))) => Selectors (t tables) where
+  selectors = selectors @(Rep (t tables 'Unresolved))
 
 
 type TableSchema = [(Text, [Field])]
 
--- TODO: How can modelSelectors be smart enough to detect if a relationship is many to many
--- and therefore needs a bridge table?
 class ModelSelectors rep where
   modelSelectors :: TableSchema
+
+  -- Generic bridge tables here
+  schema :: TableSchema
+  default schema :: TableSchema
+  -- TODO: We need to comb through this list, and if we find a two-way relationship, then we found a many to many
+  -- If we find the many to many, we need to remove the field from each of the tables, and then add a new table with those values.
+  schema = filter (const True) $ modelSelectors @rep
 
 instance ModelSelectors U1 where
   modelSelectors = []
@@ -111,12 +126,14 @@ instance (Selector s, KnitRecord (tables :: Mode -> *) t, Selectors (Rep (t tabl
 instance (ModelSelectors a, ModelSelectors b) => ModelSelectors (a :*: b) where
   modelSelectors = modelSelectors @a ++ modelSelectors @b
 
+instance ModelSelectors (model 'Unresolved) => ModelSelectors model where
+  modelSelectors = modelSelectors @(model 'Unresolved)
+
 instance ModelSelectors (Rep a) => ModelSelectors a where
   modelSelectors = modelSelectors @(Rep a)
 
-  
-class TypeName rep where
-  typename :: Text
+-- class TypeName rep where
+  -- typename :: Text
 
-instance (Datatype f) => TypeName (M1 D f x) where
-  typename = Text.pack $ datatypeName (undefined :: M1 D f x a)
+-- instance (Datatype f) => TypeName (M1 D f x) where
+  -- typename = Text.pack $ datatypeName (undefined :: M1 D f x a)
